e8aed43cba514857555004793392ad13d92b4d03
LucasIBM <lucashnoliveira@gmail.com>
2014-11-25 04:15:34 -0200

    Inseridos comentário na classe Agua para revisão do colega.

--- src/battleship/Agua.java
+++ src/battleship/Agua.java
@@ -5,29 +5,52 @@
 package battleship;
 
 /**
- *
+ * Classe para objetos do tipo Água onde estão contidos atributos e métodos para
+ * o mesmo.
+ * 
  * @author Lucas Henrique, Yuri Matheus
  */
-public class Agua implements Tabuleiro{
+public class Agua implements Tabuleiro {
 
 	protected boolean visible;
-    @Override
-    public String mostrar() {
-    	if (this.visible) {
-    		return "~";
+
+	/**
+	 * Metodo para retorno do símbolo relacionado ao objeto água em determinado
+	 * instante do jogo, caso a posicao esteja visível ao jogador será mostrado
+	 * "~" senão será mostrado "-"
+	 * 
+	 * @return String - símbolo do objeto.
+	 */
+	@Override
+	public String mostrar() {
+		if (this.visible) {
+			return "~";
 		}
-    	return "-";
-    }
+		return "-";
+	}
 
-    @Override
-    public int getType() {
-        return 0;
-    }
+	/**
+	 * Metodo para retorno do valor relacionado ao objeto água.
+	 * 
+	 * @return int - valor referente ao objeto água (0)
+	 */
+	@Override
+	public int getType() {
+		return 0;
+	}
 
+	/**
+	 * Metodo que modifica o valor do atributo visible, para deixar o símbolo do
+	 * objeto visivel ao jogador.
+	 * 
+	 * @param booelan
+	 *            -true or false
+	 * 
+	 */
 	@Override
 	public void setVisibility(boolean flag) {
-    	this.visible = flag;
-		
+		this.visible = flag;
+
 	}
-    
+
 }
