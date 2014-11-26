3e9943fbd08adeb600b6a2cce5fae1b1a403caba
LucasIBM <lucashnoliveira@gmail.com>
2014-11-26 03:15:08 -0200

    Acrescentados comentários no modelo JavaDoc nas classes BombaDestruidora, Bomba Reveladora, DistribuirAutomatico e DistribuirManual.

--- src/battleship/BombaDestruidora.java
+++ src/battleship/BombaDestruidora.java
@@ -5,29 +5,41 @@
 package battleship;
 
 /**
- *
- * @author Yuri
+ * Classe que descreve comportamento de objetos do tipo BombaDestruidora onde
+ * estão contidos métodos e atributos para o mesmo. Objetos desta classe decoram
+ * o tabuleiro.
+ * 
+ * @author Lucas Henrique, Yuri Matheus
  */
-public class BombaDestruidora extends TabuleiroDecorator{
-	
+public class BombaDestruidora extends TabuleiroDecorator {
+
 	private String estado = "";
+
+	public BombaDestruidora(Tabuleiro posicaoDecorada) {
+		super(posicaoDecorada);
+		super.setVisibility(true);
+		atirar();
+	}
+	/**
+	 * Metodo para retorno do símbolo após o lançamento de uma Bomba Destruidora 
+	 * em determinada posição.
+	 * @return String - símbolo do objeto.
+	 */
+	@Override
+	public String mostrar() {
+		return estado;
+	}
+	/**
+	 * Metodo que atira Bomba Destruidora em determina posição. Ao atirar é verificado
+	 * qual o tipo da posicao atual, caso não tenha embarcação o estado retorna "*", caso 
+	 * tenha alguma embarcação o estado retorna "X".
+	 */
 	
-    public BombaDestruidora (Tabuleiro posicaoDecorada){
-        super(posicaoDecorada);
-        super.setVisibility(true);
-        atirar();
-    }
-    
-    @Override
-    public String mostrar(){
-        return estado;
-    }
-    
-    public void atirar(){
-    	if(super.getType() > 0 && super.getType() < 6){
-    		estado = "X";
-    	}else{
-    		estado = "*";
-    	}
-    }
+	public void atirar() {
+		if (super.getType() > 0 && super.getType() < 6) {
+			estado = "X";
+		} else {
+			estado = "*";
+		}
+	}
 }
--- src/battleship/BombaReveladora.java
+++ src/battleship/BombaReveladora.java
@@ -1,5 +1,12 @@
 package battleship;
 
+/**
+ * Classe que descreve comportamento do tipo Bomba Reveladora. Ao ser lancada
+ * bomba reveladora o simbolo da posição é revelado ao jogador.
+ * 
+ * @author Lucas Henrique, Yuri Matheus
+ */
+
 public class BombaReveladora extends TabuleiroDecorator {
 
 	public BombaReveladora(Tabuleiro posicaoDecorada) {
