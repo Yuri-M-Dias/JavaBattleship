892b0527ba795b6d0c42d25774d42ac051bb70a6
LucasIBM <lucashnoliveira@gmail.com>
2014-11-26 01:32:29 -0200

    Classes do strategy com documentacao pronta

--- src/battleship/BarcoTres.java
+++ src/battleship/BarcoTres.java
@@ -5,14 +5,22 @@
 package battleship;
 
 /**
+ * Classe para objetos do tipo Barco que ocupa 3 posicoes onde estão contidos
+ * atributos e métodos para o mesmo. Objetos desta classe decoram o tabuleiro.
  *
- * @author alunoinf
+ * @author Lucas Henrique, Yuri Matheus
  */
 public class BarcoTres extends TabuleiroDecorator{
     public BarcoTres (Tabuleiro posicaoDecorada){
         super(posicaoDecorada);
     }
-    
+    /**
+	 * Metodo para retorno do símbolo relacionado ao objeto Barco Tres em
+	 * determinado instante do jogo, caso a posicao esteja visível ao jogador
+	 * será mostrado "3" senão será mostrado "-"
+	 * 
+	 * @return String - símbolo do objeto.
+	 */
     @Override
     public String mostrar(){
     	if (super.visible) {
@@ -20,6 +28,11 @@ public class BarcoTres extends TabuleiroDecorator{
 		}
     	return "-";
     }
+    /**
+	 * Metodo para retorno do valor relacionado ao objeto BarcoTres.
+	 * 
+	 * @return int - valor referente ao objeto BarcoTres (3)
+	 */
     
     @Override
     public int getType(){
